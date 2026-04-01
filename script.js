2b40004243c8f8c23f767f6e9196c4b4571dee5f
Adhyatm2717 <adhyatmmudgal@gmail.com>
2026-04-01 23:54:27 +0530

    Added adding books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const { createElement } = require("react");
+
 const topBooks = [
   "Atomic Habits",
   "The Psychology of Money",
@@ -13,27 +15,72 @@ const topBooks = [
   "1984",
   "Dune",
   "The Pragmatic Programmer",
-  "The Subtle Art of Not Giving a F*ck"
+  "The Subtle Art of Not Giving a F*ck",
 ];
 
+// let card = document.getElementsByClassName("card")[0]
 
-const bookCover_id = []
+// <div class="card">
+//             <div class="bookImage"></div>
+//             <div class="bookAndAuthorName"></div>
+// </div>
 
-for (const book of topBooks){
-    let query = book.toLowerCase().split(" ").join("+")
-    bookCover_id.push(query)
+let card;
+
+let makeCard = () => {
+  card = document.createElement("div");
+
+  card.classList.add("card");
+
+  let bookImage = document.createElement("div");
+
+  bookImage.classList.add("bookImage");
+
+  card.appendChild(bookImage);
+
+  let bookAndAuthorName = document.createElement("div");
+
+  bookAndAuthorName.classList.add("bookAndAuthorName");
+
+  bookImage.style.backgroundImage = `url("assests/817HaeblezL._AC_UF1000,1000_QL80_.jpg")`;
+
+  card.appendChild(bookAndAuthorName);
+};
+
+let booksDiv = document.getElementById("books");
+
+async function BookCoverID() {
+  let res = await fetch(`https://covers.openlibrary.org/b/id/{cover_i}-M.jpg`);
+  let data = await res.json();
+}
+
+for (i = 0; i < 14; i++) {
+  makeCard()
+//   console.log(card)
+  booksDiv.appendChild(card);
+}
+
+booksDiv.appendChild(card);
+
+// console.log(booksDiv)
+
+const bookCover_id = [];
+
+for (const book of topBooks) {
+  let query = book.toLowerCase().split(" ").join("+");
+  bookCover_id.push(query);
 }
 // console.log(bookCover_id)
 
 // let bookSearchUrl = "https://openlibrary.org/search.json?q=${book}"
 
-async function bookSearch(url){
-    let res = await fetch(url)
-    let data = await res.json()
-    console.log(data)
+async function bookSearch(book) {
+  let res = await fetch(`https://openlibrary.org/search.json?q=${book}`);
+  let data = await res.json();
+  data.docs.forEach((e) => {
+    card(e.title, e.author, e.cover_i);
+  });
 }
+bookSearch("the+alchemist");
 
-bookCover_id.map((book)=>{
-    let details = bookSearch(`https://openlibrary.org/search.json?q=${book}`)
-    console.log(details)
-})
+console.log(bookCover_id);
